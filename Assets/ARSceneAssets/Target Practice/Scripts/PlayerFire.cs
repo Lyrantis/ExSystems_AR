507e15f7408fdda2188820ed01e224acd96bcfe2
BenEdwards-0612 <112256392+BenEdwards-0612@users.noreply.github.com>
2024-05-22 10:30:42 +0100

    Made targets rotate
    Could not fix pos

--- Assets/ARSceneAssets/Target Practice/Scripts/PlayerFire.cs
+++ Assets/ARSceneAssets/Target Practice/Scripts/PlayerFire.cs
@@ -8,11 +8,29 @@ public class PlayerFire : MonoBehaviour
     [SerializeField]
     GameObject _projectilePrefab;
 
+    [SerializeField]
+    float shotDelay = 0.1f;
+
+    private bool canFire = true;
+
+    IEnumerator FireDelay()
+    {
+        yield return new WaitForSeconds(shotDelay);
+
+        canFire = true;
+    }
+
     public void Fire(InputAction.CallbackContext touch)
     {
-        Ray dir = Camera.main.ScreenPointToRay(touch.ReadValue<Vector2>());
+        if (canFire)
+        {
+            Ray dir = Camera.main.ScreenPointToRay(touch.ReadValue<Vector2>());
 
-        GameObject temp = Instantiate(_projectilePrefab, Camera.main.transform.position, Quaternion.identity);
-        temp.GetComponent<Rigidbody>().AddForce(dir.direction * 10.0f, ForceMode.Impulse);
+            GameObject temp = Instantiate(_projectilePrefab, Camera.main.transform.position, Quaternion.identity);
+            temp.GetComponent<Rigidbody>().AddForce(dir.direction * 10.0f, ForceMode.Impulse);
+
+            canFire = false;
+            StartCoroutine(FireDelay());
+        }
     }
 }
--- Assets/ARSceneAssets/Target Practice/Scripts/Target.cs
+++ Assets/ARSceneAssets/Target Practice/Scripts/Target.cs
@@ -2,19 +2,38 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class Target : MonoBehaviour
 {
     int points = 10;
 
+    //ARAnchor tmp;
+
     private void Awake()
     {
         transform.parent = null;
     }
+
+    //private void Start()
+    //{
+    //    //tmp = gameObject.AddComponent<ARAnchor>();
+    //}
+
+    private void Update()
+    {
+        //if (tmp && !tmp.pending)
+        //{
+        //    Debug.Log("ARAnchor tracking for target");
+        //}
+
+        transform.RotateAround(new Vector3(0, 0, 0), new Vector3(0, 1, 0), 1.0f);
+    }
+
     public event Action<GameObject, int> OnDeath;
     public void OnHit()
     {
-        OnDeath.Invoke(gameObject, points);
+        OnDeath?.Invoke(gameObject, points);
         Destroy(gameObject);
     }
 }
